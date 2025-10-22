60657359c521fc54265a62c0a5cab288d649ff78
mercykilonzo <mwikalimercykilonzo@gmail.com>
2025-10-22 11:18:21 +0300

    updated allure requirements

--- conftest.py
+++ conftest.py
@@ -5,6 +5,12 @@ from dotenv import load_dotenv
 from src.config import Config
 from src.helpers import AuthHelper
 
+import collections
+if not hasattr(collections, 'Mapping'):
+    import collections.abc
+    collections.Mapping = collections.abc.Mapping
+
+
 load_dotenv()
 
 @pytest.fixture(scope="session")
